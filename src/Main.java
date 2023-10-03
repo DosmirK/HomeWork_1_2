cddd027f36a8dc2d09de7a0949eeb54a034cbe72
DosmirK <dosmir017@gmail.com>
2023-10-03 18:07:02 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1: Вывести приглашение ввести имя.
+        System.out.println("Введите ваше имя: ");
+
+        // 2: Считать имя пользователя, введенное с клавиатуры.
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        // 3: Поздороваться с пользователем по введенному имени.
+        System.out.println("Привет, " + userName + "!");
+
+
+
+    }
+}
